75021a5035971e019aecae201013432f3c657ff3
{iurii.troinikov} <yurkel12@gmail.com>
2022-01-08 13:24:03 +0200

    Lesson-9-Mobile-first in action-Preprocessors-Automation: Add styles and fonts.

--- src/Blog/Cms/view/home.php
+++ src/Blog/Cms/view/home.php
@@ -4,21 +4,21 @@
         <div class="post-list">
             <div class="post">
                 <a href="/post-1-url" title="Post 1">
-                    <img src="/post-placeholder.jpg" alt="Post 1" width="200"/>
+                    <img src="post-placeholder.jpg" alt="Post 1" width="200"/>
                 </a>
                 <a href="/post-1-url" title="Post 1">Post 1</a>
                 <button type="button">Read</button>
             </div>
             <div class="post">
                 <a href="/post-2-url" title="Post 2">
-                    <img src="/post-placeholder.jpg" alt="Post 2" width="200"/>
+                    <img src="post-placeholder.jpg" alt="Post 2" width="200"/>
                 </a>
                 <a href="/post-2-url" title="Post 2">Post 2</a>
                 <button type="button">Read</button>
             </div>
             <div class="post">
                 <a href="/post-3-url" title="Post 3">
-                    <img src="/post-placeholder.jpg" alt="Post 3" width="200"/>
+                    <img src="post-placeholder.jpg" alt="Post 3" width="200"/>
                 </a>
                 <a href="/post-3-url" title="Post 3">Post 3</a>
                 <button type="button">Read</button>
