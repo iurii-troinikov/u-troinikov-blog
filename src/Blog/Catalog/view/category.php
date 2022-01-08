792aded3bd16e5cbcdfd8d10e71aa054e40c4543
{iurii.troinikov} <yurkel12@gmail.com>
2022-01-08 13:13:20 +0200

    Lesson-9-Mobile-first in action-Preprocessors-Automation: css, images minification and watch task.

--- src/Blog/Catalog/view/category.php
+++ src/Blog/Catalog/view/category.php
@@ -7,7 +7,7 @@
         <?php foreach ($block->getCategoryPosts() as $post) : ?>
             <div class="post">
                 <a href="/<?= $post->getUrl()?>" title="<?= $post->getName()?>">
-                    <img src="/post-placeholder.jpg" alt="<?= $post->getName()?>" width="200"/>
+                    <img src="post-placeholder.jpg" alt="<?= $post->getName()?>" width="200"/>
                 </a>
                 <a href="/<?= $post->getUrl()?>" title="<?= $post->getName()?>"><?= $post->getName()?></a>
                 <button type="button">Read</button>
