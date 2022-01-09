367bd819027bdf97a43ee41d5660528c45317088
{iurii.troinikov} <yurkel12@gmail.com>
2022-01-09 11:13:11 +0200

    Lesson-9-Mobile-first in action-Preprocessors-Automation: Added the fontawesome icons and styles for the Recently Viewed Posts slider

--- src/Blog/Cms/view/home.php
+++ src/Blog/Cms/view/home.php
@@ -1,27 +1,40 @@
 <!-- @TODO: Implement recently viewed posts -->
 <section title="Recently Viewed Posts">
     <h2>Recently Viewed Posts</h2>
-        <div class="post-list recently-viewed-slider">
+    <div class="post-list recently-viewed-slider-wrapper">
+        <div class="recently-viewed-slider">
             <div class="post">
-                    <a href="/post-1-url" title="post 1" class="post-item-image">
-                        <img src="post-placeholder.jpg" alt="post 1" width="200"/>
-                    </a>
-                    <a href="/post-1-url" title="post 1" class="post-item-title">Post 1</a>
-                    <button type="button" class="add-to-cart-button button-hover">Read</button>
+                <a href="/post-1-url" title="Post 1" class="post-item-image">
+                    <img src="post-placeholder.jpg" alt="Post 1"/>
+                </a>
+                <a href="/post-1-url" title="Post 1" class="post-item-title">Post 1</a>
+
+                <button type="button" class="add-to-cart-button button-hover">Read</button>
             </div>
             <div class="post">
-                    <a href="/post-2-url" title="post 2" class="post-item-image">
-                        <img src="post-placeholder.jpg" alt="post 2" width="200"/>
-                    </a>
-                    <a href="/post-2-url" title="post 2" class="post-item-title">Post 2</a>
-                    <button type="button" class="add-to-cart-button button-hover">Read</button>
+                <a href="/post-2-url" title="Post 2" class="post-item-image">
+                    <img src="post-placeholder.jpg" alt="Post 2"/>
+                </a>
+                <a href="/post-2-url" title="Post 2" class="post-item-title">Post 2</a>
+
+                <button type="button" class="add-to-cart-button button-hover">Read</button>
             </div>
             <div class="post">
-                    <a href="/post-3-url" title="post 3" class="post-item-image">
-                        <img src="post-placeholder.jpg" alt="post 3" width="200"/>
-                    </a>
-                    <a href="/post-3-url" title="post 3" class="post-item-title">Post 3</a>
-                    <button type="button" class="add-to-cart-button button-hover">Read</button>
+                <a href="/post-3-url" title="Post 3" class="post-item-image">
+                    <img src="post-placeholder.jpg" alt="Post 3"/>
+                </a>
+                <a href="/post-3-url" title="Post 3" class="post-item-title">Post 3</a>
+
+                <button type="button" class="add-to-cart-button button-hover">Read</button>
             </div>
         </div>
+        <button class="slider-control-prev slider-control button-hover" type="button">
+            <span class="slider-control-prev-icon"><i class="fas fa-chevron-left"></i></span>
+            <span class="slider-control-prev-title">Previous</span>
+        </button>
+        <button class="slider-control-next slider-control button-hover" type="button">
+            <span class="slider-control-next-icon"><i class="fas fa-chevron-right"></i></span>
+            <span class="slider-control-prev-title">Next</span>
+        </button>
+    </div>
 </section>
